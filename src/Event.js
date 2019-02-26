6e0399255632c78fb1b25cfc5e64c020f96e4e8b
euZebe <euzebe@axiome.me>
2019-02-26 11:04:18 +0100

    Initial commit

---
+++ src/Event.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import moment from "moment";
+
+export default class Event extends React.Component {
+  static propTypes = {
+    dateFrom: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    link: PropTypes.string,
+    dateTo: PropTypes.string
+  };
+
+  formatDate = (dateFrom, dateTo) =>
+    dateTo
+      ? `Du ${moment(dateFrom).format("DD MMMM")} au ${moment(dateTo).format(
+          "DD MMMM"
+        )}`
+      : `Le ${moment(dateFrom).format("DD MMMM")}`;
+
+  truncate = text =>
+    text.length > 220 ? `${text.substring(0, 40)}...Lire la suite` : text;
+
+  render() {
+    const { dateFrom, title, location, description, link, dateTo } = this.props;
+    return (
+      <div className="event">
+        <div className="eventTitle">{title}</div>
+        <div className="eventSubtitle">
+          {this.formatDate(dateFrom, dateTo)} - {location}
+        </div>
+        <div className="eventDescription">{this.truncate(description)}</div>
+      </div>
+    );
+  }
+}
---
+++ src/example.js
@@ -0,0 +1,60 @@
+import React from "react";
+import DayPicker from "react-day-picker";
+import "react-day-picker/lib/style.css";
+
+// Include the locale utils designed for moment
+import MomentLocaleUtils from "react-day-picker/moment";
+import moment from "moment";
+
+// Make sure moment.js has the required locale data
+import "moment/locale/fr";
+
+import Event from "./Event";
+import "./styles.css";
+
+import data from "./events.json";
+
+const eventDays = data.events.map(e => new Date(e.dateFrom));
+
+// TODO: handle dateTo for eventDays
+
+function isEventForDate(event, selectedDay) {
+  const dateTo = event.dateTo ? moment(event.dateTo).startOf("day") : undefined;
+  const dateFrom = moment(event.dateFrom).startOf("day");
+  const selection = moment(selectedDay).startOf("day");
+  return dateTo
+    ? dateTo >= selection && dateFrom <= selection
+    : dateFrom === selection;
+}
+
+export default class LocalizedExample extends React.Component {
+  state = { selectedDay: undefined };
+
+  getEvents = () => {
+    const { selectedDay } = this.state;
+    return data.events.map(e => <Event {...e} />);
+  };
+
+  handleDayClick = selectedDay => {
+    console.log(selectedDay);
+    this.setState({ selectedDay });
+  };
+
+  render() {
+    const { selectedDay } = this.state;
+    return (
+      <div>
+        <DayPicker
+          localeUtils={MomentLocaleUtils}
+          selectedDays={eventDays}
+          locale="fr"
+          showOutsideDays
+          onDayClick={this.handleDayClick}
+          fromMonth={new Date()}
+        />
+
+        <div>{this.getEvents()}</div>
+      </div>
+    );
+  }
+}
