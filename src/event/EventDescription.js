2d5acd54a1a1adba565dda29ab9e9cb6b5922eef
euZebe <euzebe@axiome.me>
2019-02-26 16:17:42 +0100

    read less

--- src/event/EventDescription.js
+++ src/event/EventDescription.js
@@ -8,27 +8,24 @@ export default class EventDescription extends React.PureComponent {
     this.setState({ folded: !folded });
   };
 
-  truncate = splittedText => {
-    return [
-      splittedText
-        .slice(0, 15)
-        .join(" ")
-        .concat("..."),
-      <div className="readMore" onClick={this.toggleFolded}>
-        Lire la suite
-      </div>
-    ];
-  };
-
   render() {
     const { text } = this.props;
     const { folded } = this.state;
 
     const splittedText = text.split(" ");
-    if (folded && splittedText.length > 15) {
-      return (
+    if (splittedText.length > 15) {
+      return folded ? (
         <div onClick={this.toggleFolded} className="eventDescription">
-          {this.truncate(splittedText)}
+          {splittedText
+            .slice(0, 15)
+            .join(" ")
+            .concat("...")}
+          <div className="readMore">Lire la suite</div>
+        </div>
+      ) : (
+        <div onClick={this.toggleFolded} className="eventDescription">
+          {text}
+          <div className="readMore">Afficher moins</div>
         </div>
       );
     }
